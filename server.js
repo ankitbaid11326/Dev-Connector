16e2d7f3233491355160eef0ed4e5579e60ab9cd
Ankit Baid Home PC <ankitbaid11326@gmail.com>
2018-06-26 00:03:33 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const port = process.env.PORT || 5000;
+
+// API Routes
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
+
+// DB Config
+const db = require('./config/keys').mongoURI;
+
+// Connect to Mongo-DB
+mongoose
+    .connect(db)
+    .then(() => console.log('MongoDB Connected'))
+    .catch(err => console.log(err));
+
+const app = express();
+
+app.get('/', (req, res) => res.send('Hello'));
+
+// Use Routes
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
+
+app.listen(port, () => console.log(`Server is up and running on port ${port}`));
