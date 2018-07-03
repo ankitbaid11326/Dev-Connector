318d3428f77ca89b9237f8435a0bf66e845c382d
Ankit Baid <ankitbaid@Ankits-MBP.Dlink>
2018-07-04 01:48:21 +0530

    User & Profile Delete routes added

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -202,4 +202,45 @@ router.post('/education', passport.authenticate('jwt', { session: false }), (req
     });
 });
 
+// @route   DELETE api/profile/experience/:exp_id
+// @desc    Delete experience from profile
+// @access  Private
+router.delete('/experience/:exp_id', passport.authenticate('jwt', { session: false }), (req, res) => {
+    Profile.findOne({ user: req.user.id })
+        .then(profile => {
+            const removeIndex = profile.experience.map(item => item.id).indexOf(req.params.exp_id);
+
+            profile.experience.splice(removeIndex, 1);
+
+            profile.save().then(profile => res.json(profile));
+        })
+        .catch(err => res.status(404).json(err));
+});
+
+// @route   DELETE api/profile/education/:exp_id
+// @desc    Delete education from profile
+// @access  Private
+router.delete('/education/:edu_id', passport.authenticate('jwt', { session: false }), (req, res) => {
+    Profile.findOne({ user: req.user.id })
+        .then(profile => {
+            const removeIndex = profile.education.map(item => item.id).indexOf(req.params.edu_id);
+
+            profile.education.splice(removeIndex, 1);
+
+            profile.save().then(profile => res.json(profile));
+        })
+        .catch(err => res.status(404).json(err));
+});
+
+// @route   DELETE api/profile/
+// @desc    Delete User and Profile
+// @access  Private
+router.delete('/', passport.authenticate('jwt', { session: false }), (req, res) => {
+    Profile.findOneAndRemove({ user: req.user.id }).then(() => {
+        User.findOneAndRemove({ _id: req.user.id })
+            .then(() => res.json({ success: true }))
+            .catch(err => res.json({ success: false }));
+    });
+});
+
 module.exports = router;
