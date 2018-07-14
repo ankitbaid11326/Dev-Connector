36da9301883b84eeeed376a64922da164cc2dd72
Ankit Baid <ankitbaid@Ankits-MacBook-Pro.local>
2018-07-15 00:48:33 +0530

    Login & Logout added

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import { Provider } from 'react-redux';
+import jwt_decode from 'jwt-decode';
+import setAuthToken from './utils/setAuthToken';
+import { setCurrentUser, logoutUser } from './actions/authActions';
 
+import { Provider } from 'react-redux';
 // Import react store
 import store from './store';
 
@@ -13,6 +16,26 @@ import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 
 import './App.css';
+
+// Check for token
+if (localStorage.jwtToken) {
+    // set auth token header auth
+    setAuthToken(localStorage.jwtToken);
+    // Decode token and get user info and exp
+    const decoded = jwt_decode(localStorage.jwtToken);
+    // Set user and isAuthenticated
+    store.dispatch(setCurrentUser(decoded));
+
+    // Check for expired token
+    const currentTime = Date.now() / 1000;
+    if (decoded.exp < currentTime) {
+        //Logout user
+        store.dispatch(logoutUser());
+        // TODO: Clear current profile
+        // Redirect to login
+        window.localStorage.href = '/login';
+    }
+}
 
 class App extends Component {
     render() {
--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,5 +1,8 @@
 import axios from 'axios';
-import { GET_ERRORS } from './types';
+import jwt_decode from 'jwt-decode';
+import setAuthToken from '../utils/setAuthToken';
+
+import { GET_ERRORS, SET_CURRENT_USER } from './types';
 
 // Register User
 export const registerUser = (userData, history) => dispatch => {
@@ -12,4 +15,46 @@ export const registerUser = (userData, history) => dispatch => {
                 payload: err.response.data
             })
         );
+};
+
+// Login - Ger User Token
+export const loginUser = userData => dispatch => {
+    axios
+        .post('/api/users/login', userData)
+        .then(res => {
+            // Save to local storage
+            const {token} = res.data;
+            // Set token to localStorage
+            localStorage.setItem('jwtToken', token);
+            // Set token to auth header
+            setAuthToken(token);
+            // Decode token to get user data
+            const decoded = jwt_decode(token);
+            // Set Current User
+            dispatch(setCurrentUser(decoded));
+        })
+        .catch(err =>
+            dispatch({
+                type: GET_ERRORS,
+                payload: err.response.data
+            })
+        );
+};
+
+// Set loggedin user
+export const setCurrentUser = decoded => {
+    return {
+        type: SET_CURRENT_USER,
+        payload: decoded
+    };
+};
+
+// Log out User
+export const logoutUser = () => dispatch => {
+    // Remove token from localStorage
+    localStorage.removeItem('jwtToken');
+    // Remove Auth header for future requests
+    setAuthToken(false);
+    // Set Current user to {} which will set isAuthenticated to false
+    dispatch(setCurrentUser({}));
 };
