840c7c0e7a1124b2361992236d95ce39686df6a4
Ankit Baid <ankitbaid@Ankits-MBP.Dlink>
2018-07-21 23:04:27 +0530

    Delete Profile added

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-import { GET_PROFILE, PROFILE_LOADING, CLEAR_CURRENT_PROFILE, GET_ERRORS } from '../actions/types';
+import { GET_PROFILE, PROFILE_LOADING, CLEAR_CURRENT_PROFILE, GET_ERRORS, SET_CURRENT_USER } from '../actions/types';
 
 // Get current profile
 export const getCurrentProfile = () => dispatch => {
@@ -33,6 +33,27 @@ export const createProfile = (profileData, history) => dispatch => {
             })
         );
 };
+
+// Delete Account & Profile
+export const deleteAccount = () => dispatch => {
+    if (window.confirm('Are you sure? This can NOT be undone!')) {
+        axios
+            .delete('/api/profile')
+            .then(res =>
+                dispatch({
+                    type: SET_CURRENT_USER,
+                    payload: {}
+                })
+            )
+            .catch(err =>
+                dispatch({
+                    type: GET_ERRORS,
+                    payload: err.response.data
+                })
+            );
+    }
+};
+
 // Profile loading
 export const setProfileLoading = () => {
     return {
